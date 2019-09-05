e9b5c32b3efb617fe99fa49539f2ba8f9fe3c76a
lucasfcm9 <lucasfcm9@gmail.com>
2019-09-05 14:45:36 -0300

    Add target.txt and update load_dataset.hs
    Co-authored-by: Lucas Vitor <lucasvitorifg@gmail.com>

--- load_dataset.hs
+++ load_dataset.hs
@@ -1,6 +1,12 @@
 import System.IO
+import Data.List
 
 main = do
-    let file_dataset = "dataset.txt"
-    contents <- readFile file_dataset
-    putStrLn contents
+
+    data_set <- readFile "dataset.txt"
+    print $ data_set
+
+    target_contents <- readFile "target.txt"
+    -- print $ target_contents
+    let a = map read $ words target_contents :: [Int]
+    print $ a
