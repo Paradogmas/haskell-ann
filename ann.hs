2b40eb56f255391ef13b6bd45e1e6c2730f9ccd5
Rafael Teodosio <rafzteodosio@gmail.com>
2019-09-09 16:51:48 -0300

    add random weight and bias

--- ann.hs
+++ ann.hs
@@ -7,6 +7,9 @@ import Data.Typeable
 import System.Random
 import Data.Ord
 
+randomList :: (Random a) => Int -> [a]
+randomList seed = randoms(mkStdGen seed)
+
 maxIndex :: Ord a => [a] -> Int
 maxIndex = fst . maximumBy (comparing snd) . zip[0..]
 
@@ -212,6 +215,12 @@ main = do
 
     -- end of testing area
 
+    -- Generate random weight and bias
+    let weigth1 = replicate 30(take 64 (randomList 40 :: [Double]))
+    let weigth2 = replicate 10(take 30 (randomList 44 :: [Double]))
+    let bias1 = take 30 (randomList 45 :: [Double])
+    let bias2 = take 10 (randomList 46 :: [Double])
+    
     -- initializing random matrix
     let w = random_generator 10
     --
