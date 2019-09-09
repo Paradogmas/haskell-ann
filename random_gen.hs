b723e5c400184e2a61da74ecc13d672436569303
Rafael Teodosio <rafzteodosio@gmail.com>
2019-09-09 19:18:07 -0300

    testing the random seeds generator

--- random_gen.hs
+++ random_gen.hs
@@ -8,12 +8,22 @@ randomList seed = randoms(mkStdGen seed)
 -- combine :: [Double] -> [Double] -> [[Double]]
 -- combine xs ys = [xs, ys]
 
+-- genRandomNumbers x: generate a list of x random numbers
+genRandomNumbers :: (Random a, Integral a) => Int -> Int -> [a]
+genRandomNumbers n seed = take n $ (randoms myGenerator) where
+    myGenerator = mkStdGen seed
+
+genRandomNumbersBetween :: Int -> Int -> (Int, Int) -> [Int]
+genRandomNumbersBetween n seed (a, b) = take n $ (randomRs (a, b) myGenerator) where
+    myGenerator = mkStdGen seed
+
 main :: IO ()
 main = do
-    let tenRandomNumbers = replicateM 1 randomIO :: IO [Int]
+    seed <- (round . (* 1000)) <$> getPOSIXTime 
     --putStrLn (show tenRandomNumbers)
-    let weigth1 = replicate 30(take 64 (randomList 40 :: [Double]))
-    let weigth2 = replicate 10(take 30 (randomList 44 :: [Double]))
-    let bias1 = take 30 (randomList 45 :: [Double])
-    let bias2 = take 10 (randomList 46 :: [Double])
-    print $ weigth1
+    let weigth1 = replicate 30(take 64 (randomList seed :: [Double]))
+    let weigth2 = replicate 10(take 30 (randomList seed :: [Double]))
+    let bias1 = take 30 (randomList seed :: [Double])
+    let bias2 = take 10 (randomList seed :: [Double])
+    --putStrLn (show numbers)
+    print $ bias2
